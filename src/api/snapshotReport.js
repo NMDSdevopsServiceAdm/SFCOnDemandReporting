f4000a96e2cabd6fafec2b9791bd998153e28241
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-07-25 14:12:03 +0100

    Introducing V7 for daily snapshot report to allow for changes in address, prov id, location id and nurse properties, plus refactoring to better handle errors.

--- src/api/snapshotReport.js
+++ src/api/snapshotReport.js
@@ -6,7 +6,7 @@ import { slackInfo, uploadToSlack, slackError } from '../common/slack';
 import { initialiseSecrets } from '../aws/secrets';
 import { initialiseSES, sendByEmail } from '../aws/ses';
 import { initialiseS3, upload } from '../aws/s3';
-import { separateEstablishments, separateWorkers, dailySnapshotReportV5, dailySnapshotReportV6 } from '../reports/dailySnapshot';
+import { separateEstablishments, separateWorkers, dailySnapshotReportV5, dailySnapshotReportV6, dailySnapshotReportV7 } from '../reports/dailySnapshot';
 import { resolveAllPostcodes } from '../model/postcode.api';
 import { findPostcode } from '../utils/findBy';
 
@@ -34,95 +34,106 @@ export const handler = async (event, context, callback) => {
       logInfo('Fetching list of estabishments by API');
       allEstablishmentsAndWorkersResponse = await allEstablishments();
 
-      logInfo('Fetching reference lookups');
-      lookups.services = await myServices();
-      lookups.jobs = await myJobs();
-      lookups.ethnicities = await myEthnicities();
-      lookups.countries = await myCountries();
-      lookups.nationalities = await myNationality();
-      lookups.recruitmentSources = await myRecruitmentSources();
-      lookups.qualifications = await myQualifications();
+      if (allEstablishmentsAndWorkersResponse.workers && Array.isArray(allEstablishmentsAndWorkersResponse.workers)) {
 
-      // now separate the establishments from all the workers
-      allEstablishmentsAndWorkersResponse.establishments = separateEstablishments(allEstablishmentsAndWorkersResponse.workers, lookups.services);
-      allEstablishmentsAndWorkersResponse.workers = separateWorkers(allEstablishmentsAndWorkersResponse.workers);
-
-      // now update all the Establishments with norhtings/eastings and latitude/longitude
-      const allPostcodesToLookup = {};
-      allEstablishmentsAndWorkersResponse.establishments.forEach(thisEstablishment => {
-        allPostcodesToLookup[thisEstablishment.PostCode] =true;
-      });
-      const referencePostcodes = Object.keys(allPostcodesToLookup);
-
-      // can only lookup 100 postcodes at a time - so break into batches of 90
-      const batchSize=90;
-      let resolvedPostcodes = [];
-      for (let thisIteration = 0, totalCount=referencePostcodes.length; thisIteration < totalCount; thisIteration=thisIteration+batchSize) {
-        const thisBatchPostcodes = referencePostcodes.slice(thisIteration, (thisIteration+batchSize) > totalCount ? totalCount : (thisIteration+batchSize));
-
-        const thesePostcodes = await resolveAllPostcodes(thisBatchPostcodes);
-        resolvedPostcodes = resolvedPostcodes.concat(thesePostcodes.postcodes);
-      }
-
-      allEstablishmentsAndWorkersResponse.establishments = allEstablishmentsAndWorkersResponse.establishments.map(thisEstablishment => {
-        const lookedUpPostcode = findPostcode(resolvedPostcodes, thisEstablishment.PostCode);
-        if (lookedUpPostcode) {
-          if (lookedUpPostcode.result) {
-              thisEstablishment.Eastings = lookedUpPostcode.result.eastings;
-              thisEstablishment.Northings = lookedUpPostcode.result.northings;
-              thisEstablishment.Longitude = lookedUpPostcode.result.longitude;
-              thisEstablishment.Latitude = lookedUpPostcode.result.latitude;
-          }
+        logInfo('Fetching reference lookups');
+        lookups.services = await myServices();
+        lookups.jobs = await myJobs();
+        lookups.ethnicities = await myEthnicities();
+        lookups.countries = await myCountries();
+        lookups.nationalities = await myNationality();
+        lookups.recruitmentSources = await myRecruitmentSources();
+        lookups.qualifications = await myQualifications();
+  
+        // now separate the establishments from all the workers
+        allEstablishmentsAndWorkersResponse.establishments = separateEstablishments(allEstablishmentsAndWorkersResponse.workers, lookups.services);
+        allEstablishmentsAndWorkersResponse.workers = separateWorkers(allEstablishmentsAndWorkersResponse.workers);
+  
+        // now update all the Establishments with norhtings/eastings and latitude/longitude
+        const allPostcodesToLookup = {};
+        allEstablishmentsAndWorkersResponse.establishments.forEach(thisEstablishment => {
+          allPostcodesToLookup[thisEstablishment.PostCode] =true;
+        });
+        const referencePostcodes = Object.keys(allPostcodesToLookup);
+  
+        // can only lookup 100 postcodes at a time - so break into batches of 90
+        const batchSize=90;
+        let resolvedPostcodes = [];
+        for (let thisIteration = 0, totalCount=referencePostcodes.length; thisIteration < totalCount; thisIteration=thisIteration+batchSize) {
+          const thisBatchPostcodes = referencePostcodes.slice(thisIteration, (thisIteration+batchSize) > totalCount ? totalCount : (thisIteration+batchSize));
+  
+          const thesePostcodes = await resolveAllPostcodes(thisBatchPostcodes);
+          resolvedPostcodes = resolvedPostcodes.concat(thesePostcodes.postcodes);
         }
+  
+        allEstablishmentsAndWorkersResponse.establishments = allEstablishmentsAndWorkersResponse.establishments.map(thisEstablishment => {
+          const lookedUpPostcode = findPostcode(resolvedPostcodes, thisEstablishment.PostCode);
+          if (lookedUpPostcode) {
+            if (lookedUpPostcode.result) {
+                thisEstablishment.Eastings = lookedUpPostcode.result.eastings;
+                thisEstablishment.Northings = lookedUpPostcode.result.northings;
+                thisEstablishment.Longitude = lookedUpPostcode.result.longitude;
+                thisEstablishment.Latitude = lookedUpPostcode.result.latitude;
+            }
+          }
+  
+          return thisEstablishment;
+        });
+  
+  
+        let csv = null;
+        logInfo(`Running daily snapshot report V${DataVersion}`);
+        switch (DataVersion) {
+          case 5:   // main service capcity/utilisatoin
+            csv = await dailySnapshotReportV5(allEstablishmentsAndWorkersResponse.establishments,
+                                              allEstablishmentsAndWorkersResponse.workers,
+                                              lookups);
+            break;
+  
+          case 6: // bulk upload Establishment and Worker data source
+            csv = await dailySnapshotReportV6(allEstablishmentsAndWorkersResponse.establishments,
+                                              allEstablishmentsAndWorkersResponse.workers,
+                                              lookups);
+            break;
 
-        return thisEstablishment;
-      });
-
-
-      let csv = null;
-      logInfo(`Running daily snapshot report V${DataVersion}`);
-      switch (DataVersion) {
-        case 5:   // main service capcity/utilisatoin
-          csv = await dailySnapshotReportV5(allEstablishmentsAndWorkersResponse.establishments,
-                                            allEstablishmentsAndWorkersResponse.workers,
-                                            lookups);
-          break;
-
-        case 6: // bulk upload Establishment and Worker data source
-          csv = await dailySnapshotReportV6(allEstablishmentsAndWorkersResponse.establishments,
-                                            allEstablishmentsAndWorkersResponse.workers,
-                                            lookups);
-          break;
-
-        default:
-          csv = await dailySnapshotReportV5(allEstablishmentsAndWorkersResponse.establishments,
-                                            allEstablishmentsAndWorkersResponse.workers,
-                                            lookups);
-
+          case 7: // Address split to Address1, ProvID, LocationID, Registered Nurse and Nurse Specialism
+            csv = await dailySnapshotReportV7(allEstablishmentsAndWorkersResponse.establishments,
+                                              allEstablishmentsAndWorkersResponse.workers,
+                                              lookups);
+            break;
+  
+          default:
+            csv = await dailySnapshotReportV5(allEstablishmentsAndWorkersResponse.establishments,
+                                              allEstablishmentsAndWorkersResponse.workers,
+                                              lookups);
+  
+        }
+        
+        //console.log(csv);
+        await uploadToSlack(csv);
+  
+        const today = (new Date()).toISOString().slice(0,10).replace(/-/g,"");
+  
+        const EXPIRY_IN_HOURS=5;
+        const establishmentUrl = await upload(`${today}-establishments.csv`, csv.establishmentsCsv, EXPIRY_IN_HOURS);
+        const workerUrl = await upload(`${today}-workers.csv`, csv.workersCsv, EXPIRY_IN_HOURS);
+  
+  
+        // upload the reference set of establishments/workers - used for reporting dashboard
+        await upload('dashboard/establishments.csv', csv.establishmentsCsv, 0);
+        await upload('dashboard/workers.csv', csv.workersCsv, 0);
+  
+        // send establishments by email
+        const recipient = process.env.EMAIL_RECIPIENT;
+        const htmlMessage = `<html><body>Today's Daily Snapshot Reports (${today}):<br/><ul><li><a href="${establishmentUrl}">Establishment</a></li><li><a href="${workerUrl}">Worker</a></ul></html>`;
+        const plainMessage = '';
+        await sendByEmail(recipient, 'Daily Snapshot Report', htmlMessage, plainMessage);
+  
+        await slackInfo(`Today's (${today}) Daily Snapshot Establishment Report: <${establishmentUrl}|establishments>`);
+        await slackInfo(`Today's (${today}) Daily Snapshot Workers Report: <${workerUrl}|workers>`);
+      } else {
+        logError("Failed to get daily snapshot data.")
       }
-      
-      //console.log(csv);
-      await uploadToSlack(csv);
-
-      const today = (new Date()).toISOString().slice(0,10).replace(/-/g,"");
-
-      const EXPIRY_IN_HOURS=5;
-      const establishmentUrl = await upload(`${today}-establishments.csv`, csv.establishmentsCsv, EXPIRY_IN_HOURS);
-      const workerUrl = await upload(`${today}-workers.csv`, csv.workersCsv, EXPIRY_IN_HOURS);
-
-
-      // upload the reference set of establishments/workers - used for reporting dashboard
-      await upload('dashboard/establishments.csv', csv.establishmentsCsv, 0);
-      await upload('dashboard/workers.csv', csv.workersCsv, 0);
-
-      // send establishments by email
-      const recipient = process.env.EMAIL_RECIPIENT;
-      const htmlMessage = `<html><body>Today's Daily Snapshot Reports (${today}):<br/><ul><li><a href="${establishmentUrl}">Establishment</a></li><li><a href="${workerUrl}">Worker</a></ul></html>`;
-      const plainMessage = '';
-      await sendByEmail(recipient, 'Daily Snapshot Report', htmlMessage, plainMessage);
-
-      await slackInfo(`Today's (${today}) Daily Snapshot Establishment Report: <${establishmentUrl}|establishments>`);
-      await slackInfo(`Today's (${today}) Daily Snapshot Workers Report: <${workerUrl}|workers>`);
       
     } catch (err) {
       // unable to get establishments
@@ -133,7 +144,6 @@ export const handler = async (event, context, callback) => {
 
     if (allEstablishmentsAndWorkersResponse && ![200,201].includes(allEstablishmentsAndWorkersResponse.status)) {
       logError('Error returning from SfC API: ', allEstablishmentsAndWorkersResponse);
-      slackError(slackError, 'Error returning from SfC API');
       return 'SfC API unavailable';
     }
 
